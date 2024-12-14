1578837ed8738999e47a8dd94777db46e87ad256
caTrixLiu <lyr20070621@gmail.com>
2024-12-14 17:53:43 +0800

    completed super structure subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/superstruct/SuperStructureSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/superstruct/SuperStructureSubsystem.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode.subsystems.superstruct;
 
+import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.ConditionalCommand;
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.arcrobotics.ftclib.command.WaitUntilCommand;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -12,9 +17,9 @@ import org.firstinspires.ftc.teamcode.utils.SuperStructure.ServoEx;
 public class SuperStructureSubsystem extends SubsystemBase {
     private final LinearMotion extend, elevator;
     private final ProfiledSuperStructure superStructure;
-    private final ServoEx intakeRot, intakeClaw, armClaw;
-    private SuperStructureState setPoint;
-
+    private final Servo intakeRotate, intakeClaw, armClaw;
+    private boolean intakeClawClosed, armClawClosed;
+    private double intakeRotAngle = 0.0;
 
     public SuperStructureSubsystem(HardwareMap hardwareMap) {
         this.extend = new LinearMotion(
@@ -39,18 +44,106 @@ public class SuperStructureSubsystem extends SubsystemBase {
                 0.05);
         final ProfiledMechanism intakeBase = new ProfiledMechanism(
                 new ServoEx(hardwareMap.get(Servo.class, "intakeBase")),
-                ;
-        final ProfiledMechanism intakeFlip = new ServoEx(hardwareMap.get(Servo.class, "intakeFlip"));
-        final ProfiledMechanism armFlip = new ServoEx(hardwareMap.get(Servo.class, "armFlip"));
+                0.8,
+                0.5);
+        final ProfiledMechanism intakeFlip = new ProfiledMechanism(
+                new ServoEx(hardwareMap.get(Servo.class, "intakeFlip")),
+                0.5,
+                0.5);
+        final ProfiledMechanism armFlip = new ProfiledMechanism(
+                new ServoEx(hardwareMap.get(Servo.class, "armFlip")),
+                0.8,
+                0.05);
         this.superStructure = new ProfiledSuperStructure(
                 new ProfiledMechanism[]{
-                        extend,
-                        intakeBase,
+                        new ProfiledMechanism(extend, 1.2, 0),
+                        intakeBase, intakeFlip,
+                        new ProfiledMechanism(elevator, 0.8, 0),
+                        armFlip
                 },
                 SuperStructureState.HOLD.positions
         );
 
-        this.intakeRot = new ServoEx();
-        this.intakeClaw = new ServoEx();
+        this.intakeRotate = hardwareMap.get(Servo.class, "intakeRotate");
+        this.intakeClaw = hardwareMap.get(Servo.class, "intakeClaw");
+        this.armClaw = hardwareMap.get(Servo.class, "armClaw");
+
+        this.intakeClawClosed = false;
+        this.armClawClosed = false;
     }
+
+    public void setIntakeClaw(boolean closed) {
+        this.intakeClawClosed = closed;
+    }
+
+    public void setArmClaw(boolean closed) {
+        this.armClawClosed = closed;
+    }
+
+    /**
+     * @param angle from -1 ~ 1
+     * */
+    public void setIntakeRotate(double angle) {
+        this.intakeRotAngle = angle;
+    }
+
+    public void requestState(SuperStructureState state) {
+        superStructure.requestPositions(state.positions);
+    }
+
+    @Override
+    public void periodic() {
+        superStructure.update();
+        extend.periodic();
+        elevator.periodic();
+
+        intakeClaw.setPosition(intakeClawClosed ? 0.8 : 0);
+        armClaw.setPosition(armClawClosed ? 0.6 : 0);
+        intakeRotate.setPosition(0.5 + intakeRotAngle * 0.5);
+    }
+
+    public boolean superStructAtReference() {
+        return this.superStructure.atReference();
+    }
+
+    public Command moveToState(SuperStructureState state) {
+        // wait for 100ms for debug
+        final Command waitForSuperStructureMovement = new WaitCommand(100)
+                .andThen(new WaitUntilCommand(this::superStructAtReference));
+        return new InstantCommand(() -> requestState(state))
+                .andThen(waitForSuperStructureMovement);
+    }
+
+    public Command closeIntakeClaw() {
+        return new ConditionalCommand(
+                new InstantCommand(() -> setIntakeClaw(true))
+                        .andThen(new WaitCommand(500)),
+                new InstantCommand(),
+                () -> !intakeClawClosed);
+    }
+
+    public Command openIntakeClaw() {
+        return new ConditionalCommand(
+                new InstantCommand(() -> setIntakeClaw(false))
+                        .andThen(new WaitCommand(500)),
+                new InstantCommand(),
+                () -> intakeClawClosed);
+    }
+
+    public Command closeArmClaw() {
+        return new ConditionalCommand(
+                new InstantCommand(() -> setArmClaw(true))
+                        .andThen(new WaitCommand(500)),
+                new InstantCommand(),
+                () -> !armClawClosed);
+    }
+
+    public Command openArmClaw() {
+        return new ConditionalCommand(
+                new InstantCommand(() -> setArmClaw(true))
+                        .andThen(new WaitCommand(500)),
+                new InstantCommand(),
+                () -> armClawClosed);
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/SuperStructure/ProfiledSuperStructure.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/SuperStructure/ProfiledSuperStructure.java
@@ -2,19 +2,18 @@ package org.firstinspires.ftc.teamcode.utils.SuperStructure;
 
 public class ProfiledSuperStructure {
     private final ProfiledMechanism[] profiledMechanisms;
-    private final double[] positionSetPoints;
 
     public ProfiledSuperStructure(ProfiledMechanism[] profiledMechanisms, double[] initialPositions) {
         assert profiledMechanisms.length == initialPositions.length
                 : "Mechanisms and Positions Length Not Match";
         this.profiledMechanisms = profiledMechanisms;
-        this.positionSetPoints = initialPositions;
     }
 
     public void requestPositions(double[] setPoints) {
-        assert setPoints.length == positionSetPoints.length
+        assert setPoints.length == profiledMechanisms.length
                 : "Mechanisms and Positions Length Not Match";
-        System.arraycopy(setPoints, 0, positionSetPoints, 0, positionSetPoints.length);
+       for (int i = 0; i < setPoints.length; i++)
+           profiledMechanisms[i].requestPosition(setPoints[i]);
     }
 
     public void update() {
