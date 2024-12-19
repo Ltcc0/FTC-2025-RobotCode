8e8f691c8b136278cd7b4d6b9ae445686045d9ea
caTrixLiu <lyr20070621@gmail.com>
2024-12-19 22:03:50 +0800

    resolved auto load-up delay bug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRobot.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.Robot;
 
 import org.firstinspires.ftc.teamcode.autos.Auto;
+import org.firstinspires.ftc.teamcode.constants.SystemConstants;
 import org.firstinspires.ftc.teamcode.subsystems.superstruct.SuperStructureSubsystem;
 import org.firstinspires.ftc.teamcode.utils.MapleLoopClock;
 
@@ -13,10 +15,12 @@ import org.firstinspires.ftc.teamcode.utils.MapleLoopClock;
 public class AutonomousRobot extends Robot {
     private static final MapleLoopClock beforeStartPeriodicClock = new MapleLoopClock(24);
     private final Auto auto;
+    private final Command autoCommand;
     private final RobotContainer robotContainer;
     public AutonomousRobot(RobotContainer robotContainer, Auto auto) {
         super();
         this.auto = auto;
+        autoCommand = auto.getAutonomousCommands(robotContainer);
         this.robotContainer = robotContainer;
 
         SuperStructureSubsystem.openWide = true;
@@ -28,7 +32,7 @@ public class AutonomousRobot extends Robot {
     }
 
     public void startAuto() {
-        super.schedule(auto.getAutonomousCommands(robotContainer));
+        autoCommand.schedule();
     }
 
     @Override
